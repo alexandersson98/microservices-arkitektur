2e689ab455149b7c51fa0698f4e626aa175ddec4
alexandersson98 <alexanderson98l@gmail.com>
2026-06-22 16:03:29 +0200

    fix: renamed openai to open api

--- backend/src/main/java/com/example/boilerroom_labb1/security/JwtUtil.java
+++ backend/src/main/java/com/example/boilerroom_labb1/security/JwtUtil.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
