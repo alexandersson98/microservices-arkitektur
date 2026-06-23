4d6d33cc7177f2a1efa5dde396794e3b22b9ed5d
alexandersson98 <alexanderson98l@gmail.com>
2026-06-23 14:19:24 +0200

    add: starter for library-service

--- library-service/src/main/java/org/example/library_service/LibraryServiceApplication.java
+++ library-service/src/main/java/org/example/library_service/LibraryServiceApplication.java
@@ -1,10 +1,15 @@
 package org.example.library_service;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 public class LibraryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryServiceApplication.class, args);
+    }
 }
+
